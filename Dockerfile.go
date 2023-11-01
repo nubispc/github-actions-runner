cb267c39a4c88f13e67eedeee297ef797d6b2180
Anastassios Nanos <ananos@nubificus.co.uk>
2023-11-01 09:04:38 +0000

    update
    Signed-off-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,43 +1,7 @@
-# FROM harbor.nbfc.io/nubificus/gh-actions-runner-base:generic
-#ARG BASE_IMAGE
-#FROM ${BASE_IMAGE}
-
 FROM nubificus_base_build
 
-# This the release tag of virtual-environments: https://github.com/actions/virtual-environments/releases
-ARG UBUNTU_VERSION=2004
-ARG VIRTUAL_ENVIRONMENT_VERSION=ubuntu20/20230109.1
-
-ENV UBUNTU_VERSION=${UBUNTU_VERSION} VIRTUAL_ENVIRONMENT_VERSION=${VIRTUAL_ENVIRONMENT_VERSION}
-
-# Set environment variable to prevent interactive installation
-ENV DEBIAN_FRONTEND=noninteractive
-
-SHELL ["/bin/bash", "-o", "pipefail", "-c"]
-
-# Install base packages.
 USER root
-RUN apt update && TZ=Etc/UTC \
-    apt-get install -y --no-install-recommends \
-    gcc \
-    g++ \
-    curl && \
-    apt-get -y clean && \
-    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Add sudo rule for runner user
-RUN echo "runner ALL= EXEC: NOPASSWD:ALL" >> /etc/sudoers.d/runner
-
-# Install build-essential and update cmake
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends software-properties-common && \
-    add-apt-repository -y ppa:ubuntu-toolchain-r/test && \
-    apt-get update && \
-    apt-get install -y --no-install-recommends gcc-10 g++-10 && \
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 && \
-    apt-get install -y --no-install-recommends build-essential cmake && \
-    apt-get -y clean && \
-    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 
 # Install Go depending on the system architecture
 ENV GO_VERSION=1.20.3
@@ -61,10 +25,6 @@ ENV GOROOT=/golang/go
 ENV GOPATH=/home/runner/go
 RUN go version
 
-# Copy scripts.
-COPY scripts/ /usr/local/bin/
-
-COPY entrypoint.sh /
 WORKDIR /home/runner
 USER runner
 ENTRYPOINT ["/entrypoint.sh"]
--- Dockerfile.tf
+++ Dockerfile.tf
@@ -1,82 +1,6 @@
-FROM ubuntu:20.04
+FROM nubificus_base_build
 
-# This the release tag of virtual-environments: https://github.com/actions/virtual-environments/releases
-ARG UBUNTU_VERSION=2004
-ARG VIRTUAL_ENVIRONMENT_VERSION=ubuntu20/20230109.1
-ARG GK_TEST=["x86_64"]
-
-ENV UBUNTU_VERSION=${UBUNTU_VERSION} VIRTUAL_ENVIRONMENT_VERSION=${VIRTUAL_ENVIRONMENT_VERSION}
-
-# Set environment variable to prevent interactive installation
-ENV DEBIAN_FRONTEND=noninteractive
-
-SHELL ["/bin/bash", "-o", "pipefail", "-c"]
-
-#weird bug
-RUN mkdir -p /tmp && chmod 777 /tmp && chmod +t /tmp
-
-# Install base packages.
-RUN apt update && TZ=Etc/UTC \
-    apt-get install -y --no-install-recommends \
-    ca-certificates \
-    sudo=1.8.* \
-    lsb-release=11.1.* \
-    software-properties-common=0.99.* \
-    gnupg-agent=2.2.* \
-    openssh-client=1:8.* \
-    make=4.*\
-    rsync \
-    wget \
-    jq=1.* \
-    gcc \
-    g++ \
-    clang \
-    llvm \
-    curl && \
-    apt-get -y clean && \
-    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Add sudo rule for runner user
-RUN echo "runner ALL= EXEC: NOPASSWD:ALL" >> /etc/sudoers.d/runner
-
-# Update git.
-RUN add-apt-repository -y ppa:git-core/ppa && \
-    apt-get update && \
-    apt-get -y install --no-install-recommends git && \
-    apt-get -y clean && \
-    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install docker cli.
-RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg > /etc/apt/trusted.gpg.d/docker.asc && \
-    echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list && \
-    apt-get update && \
-    apt-get install -y --no-install-recommends docker-ce-cli=5:20.10.* && \
-    apt-get -y clean && \
-    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install rust using rustup
-RUN curl https://sh.rustup.rs -sSf | sh -s -- -y
-
-# Add Kitware APT repository for updated CMake version
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends \
-    apt-transport-https ca-certificates gnupg && \
-    apt-key adv --fetch-keys 'https://apt.kitware.com/keys/kitware-archive-latest.asc' && \
-    echo 'deb https://apt.kitware.com/ubuntu/ focal main' > /etc/apt/sources.list.d/kitware.list && \
-    apt-get update
-
-
-# Install build-essential and update cmake
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends software-properties-common && \
-    add-apt-repository -y ppa:ubuntu-toolchain-r/test && \
-    apt-get update && \
-    apt-get install -y --no-install-recommends gcc-10 g++-10 && \
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 && \
-    apt-get install -y --no-install-recommends build-essential cmake && \
-    apt-get -y clean && \
-    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
+USER root
 
 # Install the required dependencies to build TensorFlow from source
 RUN apt-get update && \
@@ -97,22 +21,6 @@ RUN apt-get update && \
 # Install the Bazel version 5.3.0
 ARG BAZEL_VERSION=5.3.0
 
-#RUN apt-get update && \
-#    apt-get install -y --no-install-recommends \
-#    curl \
-#    gnupg \
-#    && curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg && \
-#    mv bazel.gpg /etc/apt/trusted.gpg.d/ && \
-#    echo "deb [arch=$(dpkg --print-architecture)] https://storage.googleapis.com/bazel-apt stable jdk1.8" > /etc/apt/sources.list.d/bazel.list && \
-#    apt-get update && \
-#    apt-get install -y --no-install-recommends bazel-${BAZEL_VERSION} && \
-#    ln -s /usr/bin/bazel-${BAZEL_VERSION} /usr/bin/bazel && \
-#    ldconfig && \
-#    apt-get -y clean && \
-#    rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-
-
 RUN export ARCH=$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/) && \
       wget https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-$ARCH && \
       chmod +x bazelisk-linux-$ARCH && \
@@ -120,7 +28,7 @@ RUN export ARCH=$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/) && \
       ln -s /usr/bin/bazel-${BAZEL_VERSION} /usr/bin/bazel
 
 
-# Clone the TensorFlow
+# Clone TensorFlow
 ARG TF_VERSION=v2.11.0
 
 RUN git clone https://github.com/tensorflow/tensorflow.git /tensorflow \
@@ -151,33 +59,11 @@ RUN git clone https://github.com/tensorflow/tensorflow.git /tensorflow \
     rm -rf /root/.cache && \
     rm -rf /tensorflow
 
-
-# Copy scripts.
-COPY scripts/ /usr/local/bin/
-
-# Install additional distro packages and runner virtual envs
-ARG VIRTUAL_ENV_PACKAGES=""
-ARG VIRTUAL_ENV_INSTALLS="basic python nodejs"
-RUN apt-get -y update && \
-    ( [ -z "$VIRTUAL_ENV_PACKAGES" ] || apt-get -y --no-install-recommends install $VIRTUAL_ENV_PACKAGES ) && \
-    . /usr/local/bin/install-from-virtual-env-helpers && \
-    for package in ${VIRTUAL_ENV_INSTALLS}; do \
-        install-from-virtual-env $package;  \
-    done && \
-    apt-get -y install --no-install-recommends gosu=1.* && \
-    apt-get -y clean && \
-    rm -rf /virtual-environments /var/cache/apt /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install runner and its dependencies.
-RUN groupadd -g 121 runner && useradd -mr -d /home/runner -u 1001 -g 121 runner && \
-    install-runner
-
 RUN wget https://raw.githubusercontent.com/tensorflow/tensorflow/v2.11.0/tensorflow/c/c_api_internal.h \
      -O /opt/tensorflow/lib/include/tensorflow/c/c_api_internal.h && \
     wget https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/core/framework/op_gen_lib.h \
      -O /opt/tensorflow/lib/include/tensorflow/core/framework/op_gen_lib.h
 
-COPY entrypoint.sh /
 WORKDIR /home/runner
 USER runner
 ENTRYPOINT ["/entrypoint.sh"]
